8740f06821dc5310838345f59e1e8893238bce49
Lisandro-bordeta <lisandrobordeta@gmail.com>
2025-12-18 17:26:38 -0300

    Actualizar api.js

--- product-frontend/src/js/api.js
+++ product-frontend/src/js/api.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:3000/api/productos'; // URL de la API local
+const apiUrl = 'http://localhost:3000/api/productos'; 
 
 const fetchProducts = async () => {
     const response = await fetch(apiUrl);
